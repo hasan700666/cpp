0b21544bd2194783b83488947e5acb34a590452a
Mohammod Hasan Al Muttaki <hasanalmuttaki@gmail.com>
2025-08-22 21:33:03 +0600

    class - 3 + uditied the class - 2 name

---
+++ 3_veriable_and_comments.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sum = 10 + 20; // veriable
+    cout << "hello world" << sum;
+    return 0;
+}
+
+// this is a single line comment
+
+/*
+this
+is
+multiline
+comment
+*/

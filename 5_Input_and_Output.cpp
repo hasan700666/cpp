af9315e0d8526fa8ac6a58a70f9553ab3d2fc1d7
Mohammod Hasan Al Muttaki <hasanalmuttaki@gmail.com>
2025-08-25 22:29:22 +0600

    Class 4 complait

---
+++ 5_Input_and_Output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << "enter the number : \n";
+    cin >> num1;
+
+    cout << "enter the number : \n";
+    cin >> num2;
+
+    cout << num1 + num2;
+
+    return 0;
+}
